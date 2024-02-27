86419020a87b7dfc19523535209f4bf2ceb4e6e6
muhammadmehdi09 <120133578+muhammadmehdi09@users.noreply.github.com>
2024-02-27 14:34:37 +0500

    Add files via upload

--- Node JS/Node JS 3/index.mjs
+++ Node JS/Node JS 3/index.mjs
@@ -1,25 +1,32 @@
-import express from "express";
+import express from 'express'
 import 'dotenv/config'
 
 const app = express()
 const PORT = process.env.PORT
+console.log(process.env.apiKey)
 
 app.get("/", (req, res) => {
     try {
-        someVariable
-        res.status(200).send({
-            message: "All is Working",
-            status: "Success"
-        })
+        RandomVar
+        res.status(200).send(
+            {
+                message: "Everything is working there is no error",
+                status: 200
+            }
+        )
     }
+
     catch (error) {
-        res.status(500).send({
-            message: "Something went wrong",
-            status: "Error"
-        })
+        res.status(500).send(
+            {
+                message: "There is an error",
+                Error: error,
+                status: 500
+            }
+        )
     }
 })
 
 app.listen(PORT, () => {
-    console.log("Server is running on localhost:" + PORT)
+    console.log("The server is running on localhost:" + PORT);
 })
